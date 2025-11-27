8e162c623c9e4a325b75ad258c3dab1c4ae29139
alexaugustobr <alexaugustoti@gmail.com>
2025-11-27 17:45:30 -0300

    aulas

---
+++ 04.06/algashop/microservices/product-catalog/src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -0,0 +1,39 @@
+package com.algaworks.algashop.product.catalog.infrastructure.persistence.product;
+
+import com.algaworks.algashop.product.catalog.application.PageModel;
+import com.algaworks.algashop.product.catalog.application.ResourceNotFoundException;
+import com.algaworks.algashop.product.catalog.application.product.query.ProductDetailOutput;
+import com.algaworks.algashop.product.catalog.application.product.query.ProductFilter;
+import com.algaworks.algashop.product.catalog.application.product.query.ProductQueryService;
+import com.algaworks.algashop.product.catalog.application.product.query.ProductSummaryOutput;
+import com.algaworks.algashop.product.catalog.application.utility.Mapper;
+import com.algaworks.algashop.product.catalog.domain.model.product.Product;
+import com.algaworks.algashop.product.catalog.domain.model.product.ProductNotFoundException;
+import com.algaworks.algashop.product.catalog.domain.model.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProductQueryServiceImpl implements ProductQueryService {
+
+    private final ProductRepository productRepository;
+    private final Mapper mapper;
+
+    @Override
+    public ProductDetailOutput findById(UUID productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException(productId));
+        return mapper.convert(product, ProductDetailOutput.class);
+    }
+
+    @Override
+    public PageModel<ProductSummaryOutput> filter(ProductFilter filter) {
+        Page<Product> products = productRepository.findAll(PageRequest.of(filter.getPage(), filter.getSize()));
+        Page<ProductSummaryOutput> productOutputs = products.map(p -> mapper.convert(p, ProductSummaryOutput.class));
+        return PageModel.of(productOutputs);
+    }
+}
