c43c29c632432b24ac8f25acea4c630580b1f551
alexaugustobr <alexaugustoti@gmail.com>
2026-01-03 15:39:10 -0300

    novas aulas

---
+++ 08.12/algashop/microservices/product-catalog/src/main/java/com/algaworks/algashop/product/catalog/application/product/query/CategoryMinimalOutput.java
@@ -0,0 +1,23 @@
+package com.algaworks.algashop.product.catalog.application.product.query;
+
+import com.algaworks.algashop.product.catalog.infrastructure.utility.Slugfier;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryMinimalOutput {
+    private UUID id;
+    private String name;
+    private Boolean enabled;
+
+    public String getSlug() {
+        return Slugfier.slugify(this.getName());
+    }
+}
