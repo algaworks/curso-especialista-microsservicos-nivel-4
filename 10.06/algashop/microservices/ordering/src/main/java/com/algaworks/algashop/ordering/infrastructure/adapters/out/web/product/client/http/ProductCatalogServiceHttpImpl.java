f05add53d5bdcff9ddf4124727e68dd61f804a1b
alexaugustobr <alexaugustoti@gmail.com>
2026-01-29 20:02:44 -0300

    novas aulas

---
+++ 10.06/algashop/microservices/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ProductCatalogServiceHttpImpl.java
@@ -0,0 +1,60 @@
+package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.client.http;
+
+import com.algaworks.algashop.ordering.core.domain.model.commons.Money;
+import com.algaworks.algashop.ordering.core.domain.model.product.Product;
+import com.algaworks.algashop.ordering.core.domain.model.product.ProductCatalogService;
+import com.algaworks.algashop.ordering.core.domain.model.product.ProductId;
+import com.algaworks.algashop.ordering.core.domain.model.product.ProductName;
+import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.exceptionhandler.BadGatewayException;
+import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.exceptionhandler.GatewayTimeoutException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.resilience.annotation.Retryable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
+
+import java.net.SocketTimeoutException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductCatalogServiceHttpImpl implements ProductCatalogService {
+
+    private final ProductCatalogAPIClient productCatalogAPIClient;
+
+    @Retryable(
+            maxRetries = 3,
+            delayString = "3s",
+            multiplier = 2,
+            includes = {GatewayTimeoutException.class, BadGatewayException.class}
+    )
+    @Override
+    public Optional<Product> ofId(ProductId productId) {
+        ProductResponse productResponse;
+        log.info("Loading product {}", productId);
+        try {
+            productResponse = productCatalogAPIClient.getById(productId.value());
+        } catch (ResourceAccessException e) {
+            throw new GatewayTimeoutException("Product Catalog API Timeout", e);
+        } catch (HttpClientErrorException.NotFound e) {
+            return Optional.empty();
+        } catch (RestClientException e) {
+            if (e.getCause() instanceof SocketTimeoutException) {
+                throw new GatewayTimeoutException("Product Catalog API Timeout", e);
+            }
+            throw new BadGatewayException("Product Catalog API Bad Gateway", e);
+        }
+
+        return Optional.of(
+                Product.builder()
+                        .id(new ProductId(productResponse.getId()))
+                        .name(new ProductName(productResponse.getName()))
+                        .inStock(productResponse.getInStock())
+                        .price(new Money(productResponse.getSalePrice()))
+                        .build()
+        );
+    }
+}
